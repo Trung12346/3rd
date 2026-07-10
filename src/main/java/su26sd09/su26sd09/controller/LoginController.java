22a67000331be7eb6afe4d968c1c1d24312fcdd8
Trung12346 <maithanhtrung9608@gmail.com>
2026-07-11 06:15:00 +0700

    bs

--- src/main/java/su26sd09/su26sd09/controller/LoginController.java
+++ src/main/java/su26sd09/su26sd09/controller/LoginController.java
@@ -4,20 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("")
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String Login(Model model){
+    public String Login(Model model,
+                        @RequestParam(value = "error", required = false) String error){
+        model.addAttribute("error", error);
         model.addAttribute("url", "/login");
         return "login";
     }
 
     @GetMapping("/nhan-su/login")
-    public String login_1(Model model)
+    public String login_1(Model model,
+                          @RequestParam(value = "error", required = false) String error)
     {
+        model.addAttribute("error", error);
         model.addAttribute("url", "/nhan-su/login");
         return "login";
     }
